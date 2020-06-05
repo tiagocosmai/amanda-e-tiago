318320cd0cec6478bb03944f74bf04e65d2aa197
Tiago Cosmai <tiagocosmai@gmail.com>
2020-06-05 12:02:26 -0300

    Updates

---
+++ precache-manifest.f54712b43a95fc8741f4a37e0bb87d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31308a9c2fac2d83beed986144b6204e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efb7bbd877637bc0c446",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "081d235cee41486b1c58",
+    "url": "/static/js/2.9f88f3f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f88f3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb7bbd877637bc0c446",
+    "url": "/static/js/main.5185598b.chunk.js"
+  },
+  {
+    "revision": "a6a4192272989586aecd",
+    "url": "/static/js/runtime-main.a97a4819.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
